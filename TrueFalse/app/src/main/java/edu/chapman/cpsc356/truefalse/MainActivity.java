5101083a5f8dc901ff5ed968992fcdc2e30d5624
ryanburns31892 <rburns@chapman.edu>
2016-09-14 19:59:22 -0700

    Cycling through questions

--- TrueFalse/app/src/main/java/edu/chapman/cpsc356/truefalse/MainActivity.java
+++ TrueFalse/app/src/main/java/edu/chapman/cpsc356/truefalse/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.chapman.cpsc356.truefalse;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,13 +9,24 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.Random;
+
 import edu.chapman.cpsc356.truefalse.models.Question;
 
 public class MainActivity extends AppCompatActivity
 {
 
     private TextView questionTextView;
-    private Question theQuestion = new Question("Life is worth it", true);
+    private View mainView;
+
+    private Question currentQuestion;
+    private Question[] questions = new Question[] {
+            new Question("Life is worth it", true),
+            new Question("A tomato is a fruit", true),
+            new Question("This class is cool", true),
+            new Question("I am a communications major", false),
+            new Question("Pizza is good 4u", false)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,7 +35,9 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         this.questionTextView = (TextView) findViewById(R.id.tv_question);
-        this.questionTextView.setText(this.theQuestion.getText());
+        this.mainView = findViewById(R.id.ll_main);
+
+        nextQuestion();
     }
 
     public void falseTapped(View view)
@@ -38,13 +52,24 @@ public class MainActivity extends AppCompatActivity
 
     private void answerTapped(boolean attemptedAnswer)
     {
-        if (this.theQuestion.getAnswer() == attemptedAnswer)
+        if (this.currentQuestion.getAnswer() == attemptedAnswer)
         {
             Toast.makeText(this, R.string.correct, Toast.LENGTH_SHORT).show();
+            this.mainView.setBackgroundColor(Color.GREEN);
         }
         else
         {
             Toast.makeText(this, R.string.wrong, Toast.LENGTH_SHORT).show();
+            this.mainView.setBackgroundColor(Color.RED);
         }
+
+        nextQuestion();
+    }
+
+    private void nextQuestion()
+    {
+        int randomIdx = new Random().nextInt(this.questions.length);
+        this.currentQuestion = this.questions[randomIdx];
+        this.questionTextView.setText(currentQuestion.getText());
     }
 }
